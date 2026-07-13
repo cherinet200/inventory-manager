5f9a949274925974e244fd9c9c75ba31441682fc
cherinet200 <cherinet200@gmail.com>
2026-07-13 22:02:10 +0300

    feat(auth): implement refresh token generation and storage

--- api/src/module/auth.ts
+++ api/src/module/auth.ts
@@ -1,13 +1,17 @@
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
+import prisma from "../db.ts";
 import jwt from "jsonwebtoken";
+import type { SignOptions } from "jsonwebtoken";
+import type { NextFunction, Request, Response } from "express";
 
 dotenv.config();
 
+interface TokenPayload {
+    id: string;
+}
 interface jwt {
     id: string;
-    name: string;
-    email: string;
 }
 
 export const hashPassword = async (password: string) =>
@@ -19,11 +23,56 @@ export const checkPassword = async (pass: string, hashedPass: string) =>
 export const createJwt = (user: jwt) => {
     const payload = {
         id: user.id,
-        name: user.name,
-        email: user.email,
     };
-    const secret_key = process.env.JWT_SECRET;
-    const token = jwt.sign(payload, secret_key!, { expiresIn: "7d" });
+    const accessToken = generateToken(payload, "15s");
+    const refreshToken = generateToken(payload, "7d");
+    saveRefreshToken(refreshToken, user.id);
+    return { accessToken: accessToken, refreshToken: refreshToken };
+};
 
-    return token;
+export const tokenRefresher = (req: Request, res: Response) => {
+    const token = req.body.token;
+
+    if (!token)
+        return res.status(400).json({ message: "Refresh token is missing!" });
+
+    try {
+        const user = jwt.verify(
+            token,
+            process.env.REFRESH_SECRET!,
+        ) as TokenPayload;
+
+        const isValid = prisma.refreshToken.findUnique({
+            where: {
+                token: token,
+            },
+        });
+
+        if (!isValid)
+            return res.status(401).json({ message: "Invalid token!" });
+
+        const accessToken = generateToken({ id: user.id }, "15s");
+        const refreshToken = generateToken({ id: user.id }, "7d");
+        saveRefreshToken(refreshToken, user.id);
+
+        return res.status(200).json({ accessToken, refreshToken });
+    } catch (err) {
+        return res.status(401).json({ message: "Invalid token!", error: err });
+    }
+};
+
+const generateToken = (payload: object, expiry: SignOptions["expiresIn"]) => {
+    return expiry === "15s"
+        ? jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn: expiry })
+        : jwt.sign(payload, process.env.REFRESH_SECRET!, { expiresIn: expiry });
+};
+
+const saveRefreshToken = async (token: string, id: string) => {
+    await prisma.refreshToken.deleteMany();
+    await prisma.refreshToken.create({
+        data: {
+            token: token,
+            userId: id,
+        },
+    });
 };
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { signIn, signUp } from "./handler/users.ts";
+import { tokenRefresher } from "./module/auth.ts";
 
 dotenv.config();
 
@@ -10,9 +11,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.json({ message: "Bello!" });
+    res.json({ message: "Welcome to my inventory manager!" });
 });
 app.post("/signup", signUp);
 app.post("/signin", signIn);
+app.post("/token", tokenRefresher);
 
 export default app;
