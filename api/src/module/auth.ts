707f52d22a1b5ebbf6d7912b61419773ad445b58
cherinet200 <cherinet200@gmail.com>
2026-07-16 21:27:19 +0300

    feat(auth): create authentication middleware

--- api/src/module/auth.ts
+++ api/src/module/auth.ts
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 import prisma from "../db.ts";
 import jwt from "jsonwebtoken";
-import type { SignOptions } from "jsonwebtoken";
+import type { JwtPayload, SignOptions } from "jsonwebtoken";
 import type { NextFunction, Request, Response } from "express";
 
 dotenv.config();
@@ -24,13 +24,13 @@ export const createJwt = (user: jwt) => {
     const payload = {
         id: user.id,
     };
-    const accessToken = generateToken(payload, "15s");
+    const accessToken = generateToken(payload, "15m");
     const refreshToken = generateToken(payload, "7d");
     saveRefreshToken(refreshToken, user.id);
     return { accessToken: accessToken, refreshToken: refreshToken };
 };
 
-export const tokenRefresher = (req: Request, res: Response) => {
+export const tokenRefresher = async (req: Request, res: Response) => {
     const token = req.body.token;
 
     if (!token)
@@ -42,7 +42,7 @@ export const tokenRefresher = (req: Request, res: Response) => {
             process.env.REFRESH_SECRET!,
         ) as TokenPayload;
 
-        const isValid = prisma.refreshToken.findUnique({
+        const isValid = await prisma.refreshToken.findUnique({
             where: {
                 token: token,
             },
@@ -51,18 +51,26 @@ export const tokenRefresher = (req: Request, res: Response) => {
         if (!isValid)
             return res.status(401).json({ message: "Invalid token!" });
 
-        const accessToken = generateToken({ id: user.id }, "15s");
+        const accessToken = generateToken({ id: user.id }, "15m");
         const refreshToken = generateToken({ id: user.id }, "7d");
         saveRefreshToken(refreshToken, user.id);
 
-        return res.status(200).json({ accessToken, refreshToken });
+        return res
+            .status(200)
+            .cookie("refresh_token", refreshToken, {
+                httpOnly: true,
+                secure: true,
+                sameSite: "strict",
+                path: "/",
+            })
+            .json({ accessToken });
     } catch (err) {
         return res.status(401).json({ message: "Invalid token!", error: err });
     }
 };
 
 const generateToken = (payload: object, expiry: SignOptions["expiresIn"]) => {
-    return expiry === "15s"
+    return expiry === "15m"
         ? jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn: expiry })
         : jwt.sign(payload, process.env.REFRESH_SECRET!, { expiresIn: expiry });
 };
@@ -75,4 +83,31 @@ const saveRefreshToken = async (token: string, id: string) => {
             userId: id,
         },
     });
+};
+
+export const Authentication = (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader)
+        return res
+            .status(401)
+            .json({ message: "Authorization header is missing" });
+
+    const token = authHeader.split(" ")[1];
+    if (!token)
+        return res.status(401).json({ message: "Bearer token is missing" });
+
+    try {
+        const claims = jwt.verify(token, process.env.JWT_SECRET!, {
+            algorithms: ["HS256"],
+        }) as JwtPayload & { id: string };
+        req.user = { id: claims.id };
+
+        next();
+    } catch (err) {
+        res.status(401).json({ message: "Invalid or expired token!", err });
+    }
 };
