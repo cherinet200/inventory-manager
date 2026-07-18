552a4f749cd53fc5b5415c736090802f172c1046
cherinet200 <cherinet200@gmail.com>
2026-07-18 13:02:04 +0300

    feat(api): support sorting, searching, filtering, and pagination for API responses

--- api/src/handler/products.ts
+++ api/src/handler/products.ts
@@ -4,30 +4,85 @@ import type { Request, Response } from "express";
 interface SortQuery {
     sortBy?: "id" | "sellingprice" | "quantity" | "expirydate";
     order?: "asc" | "desc";
+    search?: string;
+    category?: string;
+    minprice?: number;
+    maxprice?: number;
+    minquantity?: number;
+    maxquantity?: number;
+    limit?: number;
+    cursor?: number;
+    page?: number;
 }
 
+const MAX_LIMIT = 30;
 const ALLOWED_SORT_FIELDS = ["id", "sellingprice", "quantity", "expirydate"];
 
 export const getProducts = async (
     req: Request<{}, {}, {}, SortQuery>,
     res: Response,
 ) => {
-    let sortBy = req.query.sortBy || "id";
-    let order = req.query.order || "desc";
+    let {
+        sortBy = "id",
+        order = "asc",
+        search = "",
+        category = undefined,
+        minprice = undefined,
+        maxprice = undefined,
+        minquantity = undefined,
+        maxquantity = undefined,
+        limit = 5,
+        cursor = undefined,
+        page = 1,
+    } = { ...req.query };
 
     if (!ALLOWED_SORT_FIELDS.includes(sortBy)) sortBy = "id";
-    if (order !== "asc" && order !== "desc") order = "desc";
+    if (order !== "asc" && order !== "desc") order = "asc";
+    if (limit > MAX_LIMIT) limit = 30;
 
-    const products = await prisma.product.findMany({
-        where: {
+    try {
+        const where: any = {
             userId: req.user!.id,
-        },
-        orderBy: {
-            [sortBy]: order,
-        },
-    });
+            name: { contains: search, mode: "insensitive" },
+            category: category,
+        };
 
-    res.status(200).json(products);
+        if (minprice || maxprice) {
+            where.sellingprice = {};
+
+            minprice && (where.sellingprice.gte = Number(minprice));
+            maxprice && (where.sellingprice.lte = Number(maxprice));
+        }
+
+        if (minquantity || maxquantity) {
+            where.quantity = {};
+
+            minquantity && (where.quantity.gte = Number(minquantity));
+            maxquantity && (where.quantity.lte = Number(maxquantity));
+        }
+
+        const products = await prisma.product.findMany({
+            where,
+            orderBy: {
+                [sortBy]: order,
+            },
+            take: Number(limit),
+            cursor: cursor ? { id: Number(cursor) } : undefined,
+            skip: cursor ? 1 : 0,
+        });
+
+        const nextCursor =
+            products.length === Number(limit)
+                ? products[Number(limit) - 1].id
+                : null;
+
+        res.status(200).json({
+            products,
+            nextCursor,
+        });
+    } catch (err) {
+        res.status(400).json({ message: "Couldn't get products", err });
+    }
 };
 
 export const createProduct = async (req: Request, res: Response) => {
@@ -43,21 +98,18 @@ export const createProduct = async (req: Request, res: Response) => {
     ) {
         return res.status(400).json({ message: "Invalid input!" });
     }
-    await prisma.product.create({
-        data: {
-            name: req.body.name,
-            category: req.body.category,
-            buyingprice: req.body.buyingprice,
-            sellingprice: req.body.sellingprice,
-            quantity: req.body.quantity,
-            unit: req.body.unit,
-            expirydate: req.body.expirydate,
-            threshold: req.body.threshold,
-            userId: req.user!.id,
-        },
-    });
+    try {
+        const product = await prisma.product.create({
+            data: { ...req.body, userId: req.user!.id },
+        });
 
-    res.status(201).json({ message: "Product created successsfully!" });
+        res.status(201).json({
+            message: "Product created successsfully!",
+            product,
+        });
+    } catch (err) {
+        res.status(400).json({ message: "Couldn't create product", err });
+    }
 };
 
 export const deleteProduct = async (req: Request, res: Response) => {
@@ -84,7 +136,7 @@ export const editProduct = async (req: Request, res: Response) => {
     if (!id) return res.status(400).json({ message: "Invalid product ID!" });
 
     try {
-        await prisma.product.update({
+        const product = await prisma.product.update({
             where: {
                 id: id,
             },
@@ -93,6 +145,7 @@ export const editProduct = async (req: Request, res: Response) => {
 
         res.json({
             message: `Product #${id} is edited successfully!`,
+            product,
         });
     } catch (err) {
         res.status(400).json({ message: "Invalid product ID!", err });
