81478821474b87c1477ef491ff1c9ab86392fd9e
cherinet200 <cherinet200@gmail.com>
2026-07-27 23:09:47 +0300

    chore(prisma): upgarde prisma from V6 to V7

--- api/src/db.ts
+++ api/src/db.ts
@@ -1,5 +1,14 @@
-import { PrismaClient } from "@prisma/client";
+import pg from "pg";
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/index.js";
 
-const prisma = new PrismaClient();
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({ adapter });
 
 export default prisma;
