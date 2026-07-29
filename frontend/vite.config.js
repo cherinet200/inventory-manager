72dfe14f8ff78898362057846a78170e926d4450
cherinet200 <cherinet200@gmail.com>
2026-07-29 15:52:53 +0300

    chore: configure tanstackRouter, tailwindcss and proxy settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,28 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import tanstackRouter from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+    plugins: [
+        tanstackRouter(),
+        react(),
+        tailwindcss(),
+        babel({ presets: [reactCompilerPreset()] }),
+    ],
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:5000",
+                changeOrigin: true,
+            },
+            "/auth": {
+                target: "http://localhost:5000",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/auth/, ""),
+            },
+        },
+    },
+});
