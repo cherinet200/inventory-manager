2359d37e60f3d6d9b9352d3a7bf588f94e3f70f8
cherinet200 <cherinet200@gmail.com>
2026-08-05 00:12:10 +0300

    feat: add authenticated route layout with sidebar, navbar and outlet

---
+++ frontend/src/routes/_auth.tsx
@@ -0,0 +1,37 @@
+import {
+    Outlet,
+    createFileRoute,
+    redirect,
+    useRouterState,
+} from "@tanstack/react-router";
+import type { User } from "../types/types.js";
+import Sidebar from "../components/sidebar.js";
+import Navbar from "../components/navbar.js";
+
+export const Route = createFileRoute("/_auth")({
+    beforeLoad({ context }) {
+        if ((context as { user: User | null }).user === null) {
+            throw redirect({
+                to: "/signin",
+            });
+        }
+    },
+    component: RouteComponent,
+});
+
+function RouteComponent() {
+    const pathname = useRouterState({
+        select: (state) => state.location.pathname,
+    });
+    return (
+        <div className="flex bg-gray-100 dark:bg-gray-900">
+            <div className="hidden lg:flex justify-center w-72 mr-0.5 bg-white dark:bg-gray-950 rounded-md shadow-sm">
+                <Sidebar currentPath={pathname} />
+            </div>
+            <div className="flex-5 h-screen overflow-auto">
+                <Navbar />
+                <Outlet />
+            </div>
+        </div>
+    );
+}
