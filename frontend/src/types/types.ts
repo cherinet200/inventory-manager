6e84f6ae36761da10990f8282d4bfb0117706367
cherinet200 <cherinet200@gmail.com>
2026-08-12 21:53:31 +0300

    types: define interfaces for form and sales data

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -14,3 +14,21 @@ export interface Product {
     expirydate: string;
     threshold: number;
 }
+
+export interface FormData {
+    productname: string;
+    category: string;
+    buyingprice: number;
+    sellingprice: number;
+    quantity: number;
+    unit: string;
+    expirydate: string;
+    threshold: number;
+}
+
+export interface SalesData {
+    productId: number;
+    quantity: number;
+    price: number;
+    total: number;
+}
