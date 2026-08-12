50bc85f6df51d4662bf1a825951c861d18c8cb74
cherinet200 <cherinet200@gmail.com>
2026-08-12 21:55:59 +0300

    feat: enhance table cells with colSpan and onClick support

--- frontend/src/components/table.tsx
+++ frontend/src/components/table.tsx
@@ -1,19 +1,31 @@
 type TdProps = {
     children: React.ReactNode;
     style?: string;
+    colSpan?: number;
+    onClick?: () => void;
+};
+type ThProps = {
+    children: React.ReactNode;
+    style?: string;
 };
 
-export const Th = ({ children }: { children: React.ReactNode }) => {
+export const Th = ({ children, style }: ThProps) => {
     return (
-        <th className="px-5 py-5 text-left text-xs font-semibold uppercase tracking-wider text-gray-500 dark:text-gray-500">
+        <th
+            className={`px-5 py-5 text-left text-xs font-semibold uppercase tracking-wider text-gray-500 dark:text-gray-500 ${style}`}
+        >
             {children}
         </th>
     );
 };
 
-export const Td = ({ children, style }: TdProps) => {
+export const Td = ({ children, colSpan, style, onClick }: TdProps) => {
     return (
-        <td className={`px-5 py-3  text-gray-700 dark:text-gray-400 ${style}`}>
+        <td
+            colSpan={colSpan}
+            className={`px-5 py-3  text-gray-700 dark:text-gray-400 ${style}`}
+            onClick={onClick}
+        >
             {children}
         </td>
     );
