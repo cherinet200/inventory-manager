b109009f3cca81dbe78f50bedf217237066fe388
cherinet200 <cherinet200@gmail.com>
2026-08-15 18:09:57 +0300

    feat(sales): add sales selection, context menu, and bulk deletion

--- frontend/src/routes/_auth.sales.tsx
+++ frontend/src/routes/_auth.sales.tsx
@@ -1,16 +1,47 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Th, Td } from "../components/table";
 import { useEffect, useState } from "react";
-import { useAuth } from "../contexts/auth";
 import { useSales } from "../contexts/sales";
+import {
+    ListFilter,
+    Trash2,
+    LoaderCircle,
+    CircleCheck,
+    BadgeDollarSign,
+    PackageOpen,
+} from "lucide-react";
+
+type ContextMenu = {
+    x: number;
+    y: number;
+    salesId: string;
+};
 
 export const Route = createFileRoute("/_auth/sales")({
     component: RouteComponent,
 });
 
 function RouteComponent() {
-    const { sales, fetchSales, pagination } = useSales();
+    const { sales, fetchSales, deleteSales, pagination } = useSales();
     const [isLoading, setIsLoading] = useState(true);
+    const [selected, setSelected] = useState<number[]>([]);
+    const [contextMenu, setContextMenu] = useState<ContextMenu | null>(null);
+
+    const SalesId: number[] = [];
+    sales.map((sale) => SalesId.push(sale.id));
+
+    const handleSalesActions = (
+        e: React.MouseEvent<HTMLTableRowElement>,
+        salesId: string,
+    ) => {
+        e.preventDefault();
+
+        setContextMenu({
+            x: e.clientX,
+            y: e.clientY,
+            salesId,
+        });
+    };
 
     useEffect(() => {
         const fetchThem = async () => {
@@ -26,64 +57,203 @@ function RouteComponent() {
     }, []);
     return (
         <div className="flex flex-col m-10 py-8 bg-white dark:bg-gray-950 rounded-md">
-            <h2 className="mb-4 py-5 px-5 text-left text-2xl text-gray-700 dark:text-gray-300">
-                Sales
-            </h2>
-            <table className="divide-y divide-gray-100 dark:divide-gray-900">
-                <thead className="sticky top-22 bg-white dark:bg-gray-950">
-                    <tr className="">
-                        <Th>Product</Th>
-                        <Th>Price</Th>
-                        <Th>Quantity</Th>
-                        <Th>Total</Th>
-                    </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-100 dark:divide-gray-900">
-                    {sales.map((sale, index) => (
-                        <tr
-                            key={index}
-                            className="hover:bg-gray-100 dark:hover:bg-gray-900"
-                        >
-                            <Td>{sale.productId}</Td>
-                            <Td>${sale.price}</Td>
-                            <Td>{sale.quantity}</Td>
-                            <Td>${sale.total}</Td>
-                        </tr>
-                    ))}
-                </tbody>
-                <tfoot
-                    className={
-                        !pagination.hasPreviousPage && !pagination.hasNextPage
-                            ? "hidden"
-                            : ""
-                    }
+            {contextMenu && (
+                <div
+                    className="fixed z-30 w-32 rounded-md shadow-lg dark:bg-gray-800"
+                    style={{
+                        left: contextMenu.x,
+                        top: contextMenu.y,
+                    }}
                 >
-                    <tr>
-                        <Td colSpan={7}>
-                            <div className="flex items-center justify-between">
-                                <button
-                                    className={`border px-5 py-2 rounded-lg ${!pagination.hasPreviousPage && "text-gray-600"}`}
-                                    onClick={async () => {
-                                        await fetchSales(pagination.page - 1);
-                                    }}
-                                    disabled={!pagination.hasPreviousPage}
-                                >
-                                    Previous
-                                </button>
-                                <button
-                                    className={`border px-5 py-2 rounded-lg ${!pagination.hasNextPage && "text-gray-600"}`}
-                                    onClick={async () => {
-                                        await fetchSales(pagination.page + 1);
-                                    }}
-                                    disabled={!pagination.hasNextPage}
-                                >
-                                    Next
-                                </button>
-                            </div>
-                        </Td>
-                    </tr>
-                </tfoot>
-            </table>
+                    <button
+                        onClick={() => {
+                            if (selected.length !== 0) {
+                                if (selected.includes(+contextMenu.salesId)) {
+                                    setContextMenu(null);
+                                } else {
+                                    setSelected((prev) => [
+                                        ...prev,
+                                        +contextMenu.salesId,
+                                    ]);
+                                }
+                            } else {
+                                setSelected((prev) => [
+                                    ...prev,
+                                    +contextMenu.salesId,
+                                ]);
+                            }
+                            setContextMenu(null);
+                        }}
+                        className="flex w-full items-center rounded-lg gap-2 px-4 py-2 hover:bg-gray-700"
+                    >
+                        <CircleCheck className="h-4 w-4" />
+                        Select
+                    </button>
+                    <button
+                        onClick={() => {
+                            // Show Parent Product of The Sale
+                            setContextMenu(null);
+                        }}
+                        className="flex w-full items-center rounded-lg gap-2 px-4 py-2 hover:bg-gray-700"
+                    >
+                        <BadgeDollarSign className="h-4 w-4" />
+                        Product
+                    </button>
+                    <button
+                        onClick={() => {
+                            deleteSales([+contextMenu.salesId]);
+                            setSelected([]);
+                            setContextMenu(null);
+                        }}
+                        className="flex w-full items-center gap-2 rounded-lg px-4 py-2 hover:text-red-600 hover:bg-gray-700"
+                    >
+                        <Trash2 className="h-4 w-4" />
+                        Delete
+                    </button>
+                </div>
+            )}
+            <div className="flex flex-col">
+                <div className="flex justify-between items-center mb-4 py-5 px-5">
+                    <h2 className="text-left text-2xl text-gray-700 dark:text-gray-300">
+                        Sales
+                    </h2>
+                    <div className="flex gap-2">
+                        <button className="flex gap-2 items-center px-2 py-2 border border-gray-600 rounded-md hover:border-gray-700 hover:text-gray-400 hover:cursor-pointer">
+                            <ListFilter size={16} /> Filters
+                        </button>
+                        <button
+                            onClick={() => {
+                                deleteSales(selected);
+                                setSelected([]);
+                            }}
+                            className={`flex gap-2 items-center px-2 py-2 border border-gray-600 rounded-md hover:border-red-600 hover:text-red-600 hover:cursor-pointer ${selected.length < 1 && "text-gray-400"}`}
+                        >
+                            <Trash2 size={16} /> Delete
+                        </button>
+                    </div>
+                </div>
+                <table className="divide-y divide-gray-100 dark:divide-gray-900">
+                    <thead className="sticky top-22 bg-white dark:bg-gray-950">
+                        <tr className="">
+                            <Th style="flex gap-4 items-center">
+                                {selected.length !== 0 && (
+                                    <input
+                                        type="checkbox"
+                                        name="check"
+                                        id="checkSales"
+                                        className="appearance-none w-3 h-3 rounded-[100px] border-2 border-gray-500 bg-black checked:border-blue-600 cursor-pointer"
+                                        checked={
+                                            selected.length === SalesId.length
+                                        }
+                                        onChange={() => {
+                                            selected.length !== SalesId.length
+                                                ? setSelected(SalesId)
+                                                : setSelected([]);
+                                        }}
+                                    />
+                                )}
+                                Product
+                            </Th>
+                            <Th>Price</Th>
+                            <Th>Quantity</Th>
+                            <Th>Total</Th>
+                        </tr>
+                    </thead>
+                    <tbody className="divide-y divide-gray-100 dark:divide-gray-900">
+                        {sales.map((sale, index) => (
+                            <tr
+                                key={index}
+                                onClick={() => {
+                                    selected.length !== 0 &&
+                                        setSelected((prev) =>
+                                            prev.includes(sale.id)
+                                                ? prev.filter(
+                                                      (id) => id !== sale.id,
+                                                  )
+                                                : [...prev, sale.id],
+                                        );
+                                }}
+                                onContextMenu={(e) =>
+                                    handleSalesActions(e, sale.id + "")
+                                }
+                                className="hover:bg-gray-100 dark:hover:bg-gray-900"
+                            >
+                                <Td style="flex gap-4 items-center">
+                                    {selected.length !== 0 && (
+                                        <input
+                                            type="checkbox"
+                                            name="check"
+                                            id={sale.id + ""}
+                                            className="appearance-none w-3 h-3 rounded-[100px] border-2 border-gray-500 bg-black checked:border-blue-600 cursor-pointer"
+                                            checked={
+                                                selected.includes(sale.id) &&
+                                                true
+                                            }
+                                            onChange={() => {
+                                                setSelected((prev) => [
+                                                    ...prev,
+                                                ]);
+                                            }}
+                                        />
+                                    )}
+                                    {sale.productId}
+                                </Td>
+                                <Td>${sale.price}</Td>
+                                <Td>{sale.quantity}</Td>
+                                <Td>${sale.total}</Td>
+                            </tr>
+                        ))}
+                    </tbody>
+                    <tfoot
+                        className={
+                            pagination &&
+                            !pagination.hasPreviousPage &&
+                            !pagination.hasNextPage
+                                ? "hidden"
+                                : ""
+                        }
+                    >
+                        <tr>
+                            <Td colSpan={7}>
+                                <div className="flex items-center justify-between">
+                                    <button
+                                        className={`border px-5 py-2 rounded-lg ${pagination && !pagination.hasPreviousPage && "text-gray-600"}`}
+                                        onClick={async () => {
+                                            pagination &&
+                                                (await fetchSales(
+                                                    pagination.page - 1,
+                                                ));
+                                        }}
+                                        disabled={
+                                            pagination
+                                                ? !pagination.hasPreviousPage
+                                                : true
+                                        }
+                                    >
+                                        Previous
+                                    </button>
+                                    <button
+                                        className={`border px-5 py-2 rounded-lg ${pagination && !pagination.hasNextPage && "text-gray-600"}`}
+                                        onClick={async () => {
+                                            pagination &&
+                                                (await fetchSales(
+                                                    pagination.page + 1,
+                                                ));
+                                        }}
+                                        disabled={
+                                            pagination
+                                                ? !pagination.hasNextPage
+                                                : true
+                                        }
+                                    >
+                                        Next
+                                    </button>
+                                </div>
+                            </Td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
         </div>
     );
 }
