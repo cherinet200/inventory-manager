91ce0f45af5267790b0f7b000314ea7fd7166fa1
cherinet200 <cherinet200@gmail.com>
2026-08-22 22:27:32 +0300

    feat(dashboard): add dynamic sales and inventory data from server

--- frontend/src/routes/_auth.dashboard.index.tsx
+++ frontend/src/routes/_auth.dashboard.index.tsx
@@ -3,7 +3,7 @@ import { Chart } from "@tanstack/charts/react";
 import { barY, defineChart } from "@tanstack/charts";
 import { scaleBand, scaleLinear, scaleOrdinal } from "d3-scale";
 import { tooltip } from "@tanstack/charts/tooltip";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import {
     ShoppingCart,
     TrendingUp,
@@ -16,6 +16,7 @@ import {
 } from "lucide-react";
 import { Td, Th } from "../components/table";
 import { index } from "d3-array";
+import { Product } from "../types/types";
 
 export const Route = createFileRoute("/_auth/dashboard/")({
     component: Dashboard,
@@ -34,36 +35,17 @@ interface OverviewComponent {
     overview: StatCard[];
 }
 
-const sales: StatCard[] = [
-    {
-        label: "Sales",
-        value: `$832`,
-        icon: ShoppingCart,
-        iconBg: "bg-blue-400/15",
-        iconColor: "text-blue-400",
-    },
-    {
-        label: "Revenue",
-        value: `$18,300`,
-        icon: TrendingUp,
-        iconBg: "bg-indigo-500/15",
-        iconColor: "text-indigo-500",
-    },
-    {
-        label: "Cost",
-        value: `$17,432`,
-        icon: CircleDollarSign,
-        iconBg: "bg-orange-400/10",
-        iconColor: "text-orange-400",
-    },
-    {
-        label: "Profit",
-        value: `$868`,
-        icon: ChartNoAxesCombined,
-        iconBg: "bg-green-400/15",
-        iconColor: "text-green-400",
-    },
-];
+interface ProductSummaryData {
+    products: number;
+    categories: number;
+}
+
+interface TopSellingProducts {
+    name: string;
+    sold: number;
+    remaining: number;
+    price: number;
+}
 
 const purchase: StatCard[] = [
     {
@@ -81,7 +63,14 @@ const purchase: StatCard[] = [
         iconColor: "text-orange-400",
     },
     {
-        label: "Return Expected",
+        label: "Revenue",
+        value: `$17,432`,
+        icon: TrendingUp,
+        iconBg: "bg-indigo-500/15",
+        iconColor: "text-indigo-500",
+    },
+    {
+        label: "Profit",
         value: `$17,432`,
         icon: ChartNoAxesCombined,
         iconBg: "bg-green-400/15",
@@ -89,47 +78,60 @@ const purchase: StatCard[] = [
     },
 ];
 
-const TopSelling = [
-    {
-        name: "Red Bull",
-        sold: 10,
-        remaining: 20,
-        price: `$100`,
-    },
-    {
-        name: "Coca Cola",
-        sold: 10,
-        remaining: 20,
-        price: `$100`,
-    },
-    {
-        name: "Food",
-        sold: 10,
-        remaining: 20,
-        price: `$100`,
-    },
-];
-
-const LowQuantity = [
-    {
-        name: "Red Bull",
-        remaining: "10 Packet",
-    },
-    {
-        name: "Coca Cola",
-        remaining: "20 Packet",
-    },
-    {
-        name: "Food",
-        remaining: "5 Packet",
-    },
-];
-
 export default function Dashboard() {
+    const [sales, setSales] = useState<StatCard[] | null>(null);
+    useEffect(() => {
+        const fetchSalesOverview = async () => {
+            const overview = await fetch("/api/getSalesOverview", {
+                credentials: "include",
+            });
+            const overviewData = await overview.json();
+
+            if (overviewData.shouldRefresh) {
+                window.location.reload();
+            }
+
+            const salesDetail: StatCard[] = [
+                {
+                    label: "Sales",
+                    value: `$${overviewData.sales}`,
+                    icon: ShoppingCart,
+                    iconBg: "bg-blue-400/15",
+                    iconColor: "text-blue-400",
+                },
+                {
+                    label: "Cost",
+                    value: `$${overviewData.cost}`,
+                    icon: CircleDollarSign,
+                    iconBg: "bg-orange-400/10",
+                    iconColor: "text-orange-400",
+                },
+                {
+                    label: "Revenue",
+                    value: `$${overviewData.revenue}`,
+                    icon: TrendingUp,
+                    iconBg: "bg-indigo-500/15",
+                    iconColor: "text-indigo-500",
+                },
+                {
+                    label: "Profit",
+                    value: `$${overviewData.profit}`,
+                    icon: ChartNoAxesCombined,
+                    iconBg: "bg-green-400/15",
+                    iconColor: "text-green-400",
+                },
+            ];
+            console.log(overviewData);
+
+            setSales(salesDetail);
+        };
+
+        fetchSalesOverview();
+    }, []);
     return (
         <div className="flex gap-8 m-8">
             <div className="flex flex-col w-[70%] gap-4">
-                <Overview overview={sales} view="Sales" />
+                {sales && <Overview overview={sales} view="Sales" />}
                 <Overview overview={purchase} view="Purchase" />
                 <TopSellingStock />
             </div>
@@ -143,14 +145,32 @@ export default function Dashboard() {
 
 const Overview = ({ view, overview }: OverviewComponent) => {
     return (
-        <div className="flex flex-col py-4 bg-white dark:bg-gray-950 rounded-md ">
-            <section className="w-full max-w-4xl px-5 py-5">
-                <h2 className="mb-8 text-[20px] font-medium font-playfair">
-                    {view} Overview
-                </h2>
+        <div className="flex flex-col bg-white dark:bg-gray-950 rounded-md  px-5 py-6.5">
+            <div className="flex items-center justify-between mb-6">
+                <section className="w-full max-w-4xl">
+                    <h2 className="text-[20px] font-medium font-playfair">
+                        {view} Overview
+                    </h2>
+                </section>
+                <select
+                    name="time-range"
+                    id={`${view}-time-range`}
+                    className="border rounded-lg px-3 py-1.5 dark:border-blue-500/50 text-blue-500/80 dark:bg-black"
+                >
+                    <option value="today">today</option>
+                    <option value="yesterday">yesterday</option>
+                    <option value="this-week">this week</option>
+                    <option value="last-week">last week</option>
+                    <option value="this-month">this month</option>
+                    <option value="last-month">last month</option>
+                    <option value="this year">this year</option>
+                    <option value="last year">last year</option>
+                </select>
+            </div>
 
-                <div className="flex items-center">
-                    {overview.map((overview, index) => {
+            <div className="flex items-center">
+                {overview.length > 0 &&
+                    overview.map((overview, index) => {
                         const Icon = overview.icon;
 
                         return (
@@ -177,20 +197,37 @@ const Overview = ({ view, overview }: OverviewComponent) => {
                                         {overview.value}
                                     </span>
 
-                                    <span className="text-[20px] font-normal text-slate-500 dark:text-slate-500">
+                                    <span className="text-[20px] font-normal text-slate-500 ">
                                         {overview.label}
                                     </span>
                                 </div>
                             </div>
                         );
                     })}
-                </div>
-            </section>
+            </div>
         </div>
     );
 };
 
 const TopSellingStock = () => {
+    const [topSelling, setTopSelling] = useState<TopSellingProducts[]>([]);
+    useEffect(() => {
+        const fetchTopSelling = async () => {
+            const res = await fetch("/api/getTopSelling", {
+                credentials: "include",
+            });
+            const data = await res.json();
+
+            if (data.shouldRefresh) {
+                window.location.reload();
+            }
+
+            setTopSelling(data.topSellingProducts);
+        };
+
+        fetchTopSelling();
+    }, []);
+
     return (
         <div className="flex flex-col py-4 bg-white dark:bg-gray-950 rounded-md overflow-x-auto">
             <section className="w-full px-5">
@@ -198,39 +235,112 @@ const TopSellingStock = () => {
                     <h2 className="text-[20px] font-medium font-playfair">
                         Top Selling Stock
                     </h2>
-                    <button className="py-2 px-4 text-blue-600 hover:bg-blue-600/25 rounded-2xl">
-                        See All
-                    </button>
+                    <div className="flex gap-2">
+                        <select
+                            name="time-range"
+                            id="top-selling-time-range"
+                            className="border rounded-lg px-3 py-1.5 dark:border-blue-500/50 text-blue-500/80 dark:bg-black"
+                        >
+                            <option value="today" className="dark:text-white">
+                                today
+                            </option>
+                            <option
+                                value="yesterday"
+                                className="dark:text-white"
+                            >
+                                yesterday
+                            </option>
+                            <option
+                                value="this-week"
+                                className="dark:text-white"
+                            >
+                                this week
+                            </option>
+                            <option
+                                value="last-week"
+                                className="dark:text-white"
+                            >
+                                last week
+                            </option>
+                            <option
+                                value="this-month"
+                                className="dark:text-white"
+                            >
+                                this month
+                            </option>
+                            <option
+                                value="last-month"
+                                className="dark:text-white"
+                            >
+                                last month
+                            </option>
+                            <option
+                                value="this year"
+                                className="dark:text-white"
+                            >
+                                this year
+                            </option>
+                            <option
+                                value="last year"
+                                className="dark:text-white"
+                            >
+                                last year
+                            </option>
+                        </select>
+                        <button className="py-2 px-4 text-blue-600 hover:bg-blue-600/25 bg-blue-600/10 rounded-2xl">
+                            See All
+                        </button>
+                    </div>
                 </div>
             </section>
-            <table className="divide-y divide-gray-100 dark:divide-gray-900">
-                <thead className="bg-white dark:bg-gray-950">
-                    <tr className="">
-                        <Th>Name</Th>
-                        <Th>Sold Quantity</Th>
-                        <Th>Remaining Quantity</Th>
-                        <Th>Price</Th>
-                    </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-100 dark:divide-gray-900">
-                    {TopSelling.map((product, index) => (
-                        <tr
-                            key={index}
-                            className="hover:bg-gray-100 dark:hover:bg-gray-900"
-                        >
-                            <Td>{product.name}</Td>
-                            <Td>{product.sold}</Td>
-                            <Td>{product.remaining}</Td>
-                            <Td>{product.price}</Td>
+            {topSelling && (
+                <table className="divide-y divide-gray-100 dark:divide-gray-900">
+                    <thead className="bg-white dark:bg-gray-950">
+                        <tr className="">
+                            <Th>Name</Th>
+                            <Th>Sold Quantity</Th>
+                            <Th>Remaining Quantity</Th>
+                            <Th>Price</Th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody className="divide-y divide-gray-100 dark:divide-gray-900">
+                        {topSelling.map((product, index) => (
+                            <tr
+                                key={index}
+                                className="hover:bg-gray-100 dark:hover:bg-gray-900"
+                            >
+                                <Td>{product.name}</Td>
+                                <Td>{product.sold}</Td>
+                                <Td>{product.remaining}</Td>
+                                <Td>{product.price}</Td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </div>
     );
 };
 
 const LowQuantityStock = () => {
+    const [lowQuantity, setLowQuantity] = useState<Product[]>([]);
+
+    useEffect(() => {
+        const response = async () => {
+            const res = await fetch("/api/getLowStocks", {
+                credentials: "include",
+            });
+            const result = await res.json();
+
+            if (result.shouldRefresh) {
+                window.location.reload();
+            }
+
+            setLowQuantity(result);
+        };
+
+        response();
+    }, []);
     return (
         <div className="flex flex-col py-4 bg-white dark:bg-gray-950 rounded-md ">
             <section className="w-full px-5 py-5">
@@ -238,34 +348,63 @@ const LowQuantityStock = () => {
                     <h2 className="text-[20px] font-medium font-playfair">
                         Low Quantity Stock
                     </h2>
-                    <button className="py-2 px-4 text-blue-600 hover:bg-blue-600/25 rounded-2xl">
-                        See All
-                    </button>
+                    {lowQuantity.length > 1 && (
+                        <button className="py-2 px-4 text-blue-600 hover:bg-blue-600/25 bg-blue-600/10 rounded-2xl">
+                            See All
+                        </button>
+                    )}
                 </div>
 
-                <div className="flex flex-col gap-4">
-                    {LowQuantity.map((product, index) => (
-                        <div className="flex justify-between items-center">
-                            <div className="flex flex-col">
-                                <div className="text-gray-300 font-bold">
-                                    {product.name}
+                {lowQuantity.length > 1 && (
+                    <div className="flex flex-col gap-4">
+                        {lowQuantity.map((product, index) => (
+                            <div
+                                key={index}
+                                className="flex justify-between items-center"
+                            >
+                                <div className="flex flex-col">
+                                    <div className="text-gray-300 font-bold">
+                                        {product.name}
+                                    </div>
+                                    <div className="text-gray-400">
+                                        Remaining Quantity:{" "}
+                                        {product.quantity + " " + product.unit}
+                                    </div>
                                 </div>
-                                <div className="text-gray-400">
-                                    Remaining Quantity: {product.remaining}
+                                <div className="px-2 py-0.5 rounded-2xl bg-red-800/20 text-red-800">
+                                    Low
                                 </div>
                             </div>
-                            <div className="px-2 py-0.5 rounded-2xl bg-red-800/25 text-red-800">
-                                Low
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                        ))}
+                    </div>
+                )}
             </section>
         </div>
     );
 };
 
 const ProductSummary = () => {
+    const [productSummary, setProductSummary] = useState<ProductSummaryData>({
+        products: 0,
+        categories: 0,
+    });
+
+    useEffect(() => {
+        const response = async () => {
+            const res = await fetch("/api/getProductSummary", {
+                credentials: "include",
+            });
+            const result = await res.json();
+
+            if (result.shouldRefresh) {
+                window.location.reload();
+            }
+
+            setProductSummary(result);
+        };
+
+        response();
+    }, []);
     return (
         <div className="flex flex-col py-4 px-5 bg-white dark:bg-gray-950 rounded-md overflow-x-auto">
             <section className="w-full">
@@ -276,23 +415,31 @@ const ProductSummary = () => {
                 </div>
             </section>
             <div className="flex justify-center divide-x divide-gray-800">
-                <div className="flex flex-col gap-1 pr-10 items-center">
-                    <div className="flex justify-between gap-15">
-                        <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-md bg-green-500/15">
+                <div className="flex flex-col gap-1 pr-6 items-center">
+                    <div className="flex items-center gap-8">
+                        <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-md bg-green-400/15">
                             <Boxes className="text-green-500" />
                         </div>
-                        <span>100</span>
+                        <span className="dark:text-slate-400 font-semibold text-[20px]">
+                            {productSummary.products}
+                        </span>
                     </div>
-                    <span>Number of Products</span>
+                    <span className="text-[18px] text-slate-500">
+                        Number of Products
+                    </span>
                 </div>
-                <div className="flex flex-col gap-1 pl-10 items-center">
-                    <div className="flex items-center gap-15">
+                <div className="flex flex-col gap-1 pl-6 items-center">
+                    <div className="flex items-center gap-8">
                         <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-md bg-indigo-500/15">
                             <Tags className="text-indigo-500" />
                         </div>
-                        <span>10</span>
+                        <span className="dark:text-slate-400 font-semibold text-[20px]">
+                            {productSummary.categories}
+                        </span>
                     </div>
-                    <span>Number of Products</span>
+                    <span className="text-[18px] text-slate-500">
+                        Number of Categories
+                    </span>
                 </div>
             </div>
         </div>
