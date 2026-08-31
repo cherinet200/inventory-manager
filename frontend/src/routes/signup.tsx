bc8cc4a368735c3d5904323cb8f2d3bb7337ac68
cherinet200 <cherinet200@gmail.com>
2026-08-31 15:13:50 +0300

    style: increase the width of sign up form

--- frontend/src/routes/signup.tsx
+++ frontend/src/routes/signup.tsx
@@ -163,7 +163,7 @@ function Signup() {
                 <img src={brandLogo} alt="Brand" />
             </div>
             <form
-                className="h-full w-97.5 flex justify-center items-center flex-col gap-8"
+                className="h-full w-120 flex justify-center items-center flex-col gap-8"
                 onSubmit={handleSubmit}
             >
                 <div className="flex justify-center items-center flex-col gap-4">
