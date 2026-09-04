133008a714f5124f2580c4f8fc31eb79851ee1c6
cherinet200 <cherinet200@gmail.com>
2026-09-04 15:16:53 +0300

    chore: update routing

--- api/src/server.ts
+++ api/src/server.ts
@@ -40,11 +40,11 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to my inventory manager!" });
 });
 
-app.post("/auth/signup", signUp);
-app.post("/auth/signin", signIn);
+app.post("/signup", signUp);
+app.post("/signin", signIn);
 
-app.post("/auth/forgotPassword", forgotPassword);
-app.post("/auth/changePassword", changePassword);
+app.post("/forgotPassword", forgotPassword);
+app.post("/changePassword", changePassword);
 
 app.use("/api", Authentication, router);
 
