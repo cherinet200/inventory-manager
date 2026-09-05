8e4091931ea74cd979564736b76e87554c3919c8
cherinet200 <cherinet200@gmail.com>
2026-09-05 21:21:48 +0300

    chore: update path for generated

--- api/src/db.ts
+++ api/src/db.ts
@@ -1,7 +1,7 @@
 import pg from "pg";
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../src/generated/prisma/index.js";
+import { PrismaClient } from "../dist/generated/prisma/index.js";
 
 const pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
